754fb3622ca7ac5542b4130a19239fe5a02cf639
Ramkumar1301investacc <ramkumar@investacc.co.in>
2024-02-24 15:53:16 +0530

    Added service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { Reservation } from '../models/reservation';
+import { ReservationModule } from './reservation.module';
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
   constructor() { }
+private reservation: Reservation[]=[];
+  
+getreservations(): Reservation[]{
+    return this.reservation
+
+}
+getReservation(id:string):Reservation|undefined{
+  return this.reservation.find(res => res.id === id);
+  
+}
+addReservation(reservation:Reservation): void{
+  this.reservation.push(reservation)
+}
+deleteReservation(id:string):void{
+  let index= this.reservation.findIndex(res => res.id === id);
+  this.reservation.splice(index,1)
+}
+
 }
